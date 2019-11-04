8d48a43c7ef66f78a6f4e169a2993c70378ff396
Vitor Chaves <vitor.chaves022@gmail.com>
2019-11-04 06:34:29 -0300

    Chrome COrrections

--- js/vayce.js
+++ js/vayce.js
@@ -21,25 +21,7 @@
     var $menu = this;
 
     return $menu.each(function(settings){
-      $menu.css({
-        position: 'relative',
-        width: 'fit-content',
-        width: '-moz-fit-content'
-      }).find("nav").css({
-        backgroundColor: defaults.bgColor,
-        width: "fit-content",
-        width: '-moz-fit-content',
-        position: 'absolute',
-        marginTop: defaults.topSpace,
-        right: 0,
-        top: 0,
-        borderRadius: defaults.borderRadius
-      }).hide().find('ul').css({
-        listStyle: 'none',
-        maxWidth: '250px',
-        width: "max-content",
-        width: '-moz-max-content'
-      }).find('li').css({
+      $menu.attr('style', "position: relative; width: fit-content; width: -moz-fit-content;").find("nav").attr('style', "background-color:" + defaults.bgColor + "; width: fit-content; width: -moz-fit-content; position: absolute; margin-top: "+defaults.topSpace+"; right: 0; top: 0; borderRadius: " + defaults.borderRadius}).hide().find('ul').attr('style', "list-style: none;  max-width: 250px; width: max-content; width: -moz-max-content").find('li').css({
         padding: defaults.itemsPadding,
         color: defaults.fgColor,
         cursor: 'pointer'
